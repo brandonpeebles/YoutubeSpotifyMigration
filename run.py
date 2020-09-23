ec0b8c8d99e2859249e24845788765e42ee55922
Brandon Peebles <peeblesbrandon@gmail.com>
2020-09-23 13:51:09 -0400

    make class methods private

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import src.migrations.YoutubeToSpotify as YoutubeToSpotify
 
 if __name__ == '__main__':
     migrator = YoutubeToSpotify.Migrator()
-    selectedPlaylist = migrator.get_playlist_from_input()
+    migrator.execute()
     # spotifyMatches = 
--- src/migrations/YoutubeToSpotify.py
+++ src/migrations/YoutubeToSpotify.py
@@ -14,8 +14,13 @@ class Migrator:
         # initialize and authenticate Spotify client instances
         self.SpotifyAPI = Spotify.Client()
         self.SpotifyAPI.authenticate()
+        self._youtube_playlist = None
+        self._spotify_matches = None
 
-    def get_playlist_from_input(self):
+    def execute(self):
+        self._get_playlist_from_input()
+    
+    def _get_playlist_from_input(self):
         """Fetch Youtube playlists for user and ask them to choose one for transferring"""
         playlists = self.YoutubeAPI.get_all_playlists()                                 # fetch the user's playlists
         question = [                                                                    # prompt user to select one
@@ -29,12 +34,13 @@ class Migrator:
         answer = prompt(question, style=custom_style_2)
         selectedIndex = answer['selectedPlaylist']
         selectedPlaylistId = playlists['items'][selectedIndex]['id']
-        return self.YoutubeAPI.get_playlist_items(selectedPlaylistId)['items']          # return all vids for selected playlist
+        self._youtube_playlist = self.YoutubeAPI.get_playlist_items(selectedPlaylistId)['items']          # return all vids for selected playlist
         
-    def get_spotify_matches(self, songsList):
+    def _get_spotify_matches(self):
         """Fetch list of spotify search results for given array of Youtube videos"""
-        
 
-    def transfer_songs(self, spotifyURIs):
+    def _confirm_spotify_matches(self):
+
+    def _transfer_songs(self):
         pass
 
