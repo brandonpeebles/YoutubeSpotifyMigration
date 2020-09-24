6ebab708327b30d74c7fa060a4bae27b9d0114b5
Brandon Peebles <peeblesbrandon@gmail.com>
2020-09-23 22:58:32 -0400

    add logo styling on run

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+from colorama import Fore, Back, Style
+from pyfiglet import Figlet
+
 import src.migrations.YoutubeToSpotify as YoutubeToSpotify
 
 if __name__ == '__main__':
+    f = Figlet(font='cybermedium')
+    print(Style.BRIGHT + Fore.YELLOW + 
+        f.renderText('Youtube : Spotify Migration') + Style.RESET_ALL)
     migrator = YoutubeToSpotify.Migrator()
     migrator.execute()
     # spotifyMatches = 
