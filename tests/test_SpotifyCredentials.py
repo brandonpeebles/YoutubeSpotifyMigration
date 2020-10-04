550e89766bf787723924afae9ca588046f39e5e3
Brandon Peebles <peeblesbrandon@gmail.com>
2020-10-03 23:32:43 -0400

    complete creds tests

--- tests/test_SpotifyCredentials.py
+++ tests/test_SpotifyCredentials.py
@@ -1,5 +1,5 @@
 import unittest
-import json, os
+import json, os, time
 import requests
 os.chdir("..")
 from src.migrations.clients.credentials.SpotifyCredentials import Credentials
@@ -25,6 +25,18 @@ class TestSpotifyCredentials(unittest.TestCase):
         self.assertEqual(self.creds.refresh_token(), "refresh_token_value")
         self.assertEqual(self.creds.auth_header(), "auth_header_value")
 
+    def test_expiry(self):
+        self.assertFalse(self.creds.expired(), "new creds should not be expired")
+        old_expiry = self.creds._expiry 
+        self.creds._set_expiry(2)
+        self.assertNotEqual(old_expiry, self.creds._expiry, "expiry should have updated")
+        time.sleep(3) # so creds expire
+        self.assertTrue(self.creds.expired(), "creds should have expired")
+        self.creds._set_expiry(3600)
+
+    def test_validity(self):
+        self.assertTrue(self.creds.valid())
+        # add test for refresh method 
 
 if __name__ == "__main__":
     unittest.main() 
